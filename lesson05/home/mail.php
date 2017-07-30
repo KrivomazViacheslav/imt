079009687a4aa2ece19d213e50ba667be2df9110
Krivomaz Viacheslav <v.krivomaz@gmail.com>
2017-07-30 12:14:38 +0300

    Doing homework

---
+++ lesson05/home/mail.php
@@ -0,0 +1,26 @@
+﻿<?php
+/*
+$to		 = "mihaskep@gmail.com";
+$subject = "Тестовое письмо"; 
+$message = ' 
+<html> 
+    <head> 
+        <title>Тестовое письмо</title> 
+    </head> 
+    <body> 
+        <p>Александр, привет! Ответь, пожалуйста, мне на это письмо.<br><br>С уважением,<br>Кривомаз Вячеслав.</p> 
+    </body> 
+</html>'; 
+
+$headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+$headers .= "From: v.krivomaz@gmail.com\r\n"; 
+
+$res = mail($to, $subject, $message, $headers); 
+
+if ($res) {
+	echo 'Ok';
+} else {
+	echo 'Not ok';
+}
+*/
+?>
