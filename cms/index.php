983613290c4e5274af678c73cda68f74a1544d62
Krivomaz Viacheslav <v.krivomaz@gmail.com>
2017-08-04 21:53:38 +0300

    Doing homework

---
+++ cms/index.php
@@ -0,0 +1,43 @@
+<?php require_once 'html/functions.php' ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Мой магазин</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+</head>
+<body>
+
+    <div class="row">
+        <div class="bg-info col-lg-12">
+            <?php viewMenu($pages) ?>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="container">
+            <div class="col-lg-4 panel panel-default">
+                <h2 class="panel-heading">Sidebar</h2>
+                <div class="">
+                    <?php
+                        $categories_tree = makeTree($categories);
+                        viewCategories($categories_tree);
+                    ?>
+                </div>
+            </div>
+            <div class="col-lg-8 panel panel-default">
+                <h2 class="panel-heading">Content</h2>
+
+                <div> <?php require_once 'html/content.php'?></div>
+            </div>
+            <div class="row col-lg-12">
+                <div class="panel-footer">
+                    <h2 class="panel-heading">Footer</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ lesson06/school/sw06.php
@@ -0,0 +1,17 @@
+﻿
+<?php
+
+print_r($_GET);
+
+?>
+
+<form>
+	<input type="text" name="name" value="">
+	<input type="number" name="age" value="">
+	<input type="submit" name="go">
+</form>
+
+
+<?php
+
+//print_r($_SERVER);
